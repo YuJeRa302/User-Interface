e97d7dd9daaf5b3a7db92d8a0f5a9615cac1eede
YuJeRa302 <71927873+YuJeRa302@users.noreply.github.com>
2022-10-14 11:55:16 +0300

    Fix 1

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,36 +1,52 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Player : MonoBehaviour
 {
-    [Header("Scripts")]
-    [SerializeField] private UpdateHealthBar _updateHealthBar;
-
     [SerializeField] private int _damage = 10;
     [SerializeField] private int _healing = 10;
 
+    private UnityEvent _healthBarUpdate = new UnityEvent();
     private int _currentHealth;
     private int _maxHealth = 100;
 
+    public event UnityAction HealthBarUpdate
+    {
+        add => _healthBarUpdate.AddListener(value);
+        remove => _healthBarUpdate.RemoveListener(value);
+    }
+
+    public bool IsChangeHealth { get; private set; }
+    public int Damage { get; private set; }
+    public int HealHealth { get; private set; }
+
     private void Awake()
     {
+        Damage = _damage;
+        HealHealth = _healing;
         _currentHealth = _maxHealth;
     }
 
-    public void TakeDamage() 
-    { 
-        if (_currentHealth - _damage >= 0) 
+    public void TakeDamage()
+    {
+        if (_currentHealth - _damage >= 0)
         {
+            IsChangeHealth = true;
             _currentHealth -= _damage;
-            _updateHealthBar.UpdateUI(_currentHealth);
         }
+
+        _healthBarUpdate.Invoke();
     }
 
-    public void Healing() 
+    public void Healing()
     {
-        if (_currentHealth + _healing <= _maxHealth) 
+        if (_currentHealth + _healing <= _maxHealth)
         {
+            IsChangeHealth = false;
             _currentHealth += _healing;
-            _updateHealthBar.UpdateUI(_currentHealth);
         }
+
+        _healthBarUpdate.Invoke();
     }
 }
--- Assets/Scripts/UI/UpdateHealthBar.cs
+++ Assets/Scripts/UI/UpdateHealthBar.cs
@@ -1,17 +1,62 @@
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UpdateHealthBar : MonoBehaviour
 {
     [SerializeField] private Slider _slider;
+    [SerializeField] private Player _player;
+
+    private IEnumerator _healthChange;
+    private int _speed = 1;
+    private float _delay = 0.1f;
 
     private void Awake()
     {
         _slider.value = _slider.maxValue;
     }
 
-    public void UpdateUI(int value) 
+    private void OnEnable()
     {
-        _slider.value = value;
+        _player.HealthBarUpdate += OnChangeHealth;
+    }
+
+    private void OnDisable()
+    {
+        _player.HealthBarUpdate -= OnChangeHealth;
+    }
+
+    public void OnChangeHealth()
+    {
+        if (_healthChange != null)
+        {
+            StopCoroutine(_healthChange);
+            _healthChange = ChangeHealth(_player.IsChangeHealth);
+            StartCoroutine(_healthChange);
+        }
+        else
+        {
+            if (_healthChange != null)
+            {
+                StopCoroutine(_healthChange);
+            }
+
+            _healthChange = ChangeHealth(_player.IsChangeHealth);
+            StartCoroutine(_healthChange);
+        }
+    }
+
+    private IEnumerator ChangeHealth(bool isChangeHealth)
+    {
+        int value = isChangeHealth ? _player.Damage : _player.HealHealth;
+
+        while (value >= 0)
+        {
+            var _changeValue = isChangeHealth ? _slider.value - value : _slider.value + value;
+            _slider.value = Mathf.MoveTowards(_slider.value, _changeValue, _speed);
+            value--;
+            yield return new WaitForSeconds(_delay);
+        }
     }
 }
